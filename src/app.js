4df4c6b75cbb48c131744b68709589a3d3c98df4
pulgazion <reimarrodri@gmail.com>
2023-01-26 17:36:30 +0000

    Generador de Dominios

--- src/app.js
+++ src/app.js
@@ -6,6 +6,18 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  let dominio = [".com", ".org", ".net", ".io"];
+  let pronoun = ["the", "our"];
+  let adj = ["great", "big"];
+  let noun = ["jogger", "racoon"];
+
+  for (let i = 0; i < pronoun.length; i++) {
+    for (let j = 0; j < noun.length; j++) {
+      for (let p = 0; p < adj.length; p++) {
+        for (let r = 0; r < dominio.length; r++) {
+          console.log(`${pronoun[i]}${noun[j]} ${adj[p]}${dominio[r]}`);
+        }
+      }
+    }
+  }
 };
